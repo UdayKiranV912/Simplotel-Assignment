4644284ac64b75fad7cee3ecf23846c53eb3b5d5
Uday KIran V <85879936+UdayKiranV912@users.noreply.github.com>
2023-12-24 13:32:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var clickMeButton = document.getElementById("clickMeButton");
+
+  clickMeButton.addEventListener("click", function () {
+    alert("Button clicked!");
+    // You can add any other actions you want to perform when the button is clicked
+  });
+});
